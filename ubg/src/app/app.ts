61d1a49ae789fe2e3e7fc906f5f9e39ad1ad5500
Richel N <rnssaketo@proton.me>
2026-01-06 18:28:42 +0000

    Remove useless imports

--- ubg/src/app/app.ts
+++ ubg/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Main } from './components/main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ubg');
+  protected readonly title = signal('Unblocked Games');
 }
