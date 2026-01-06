1b1b86595cf311f91ef22ff96e4e9b9ab4cd91fd
Richel N <rnssaketo@proton.me>
2026-01-06 18:28:22 +0000

    Change files of app component in other folders

--- projects/src/app/app.ts
+++ projects/src/app/app.ts
@@ -1,13 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Card } from './classes/card';
-import { Linkcard } from './components/linkcard/linkcard';
-import { CardList } from './service/card-list';
+import { Component, signal } from '@angular/core';
 import { Main } from './components/main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Main],
+  imports: [Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
