167e580ec1d78d414315e0bd4ddfce22a17efbb3
Richel N <rnssaketo@proton.me>
2026-01-12 10:36:43 -0500

    Remove Arsenic because the proxy does not exist anymore

--- ub/src/app/service/card-list.ts
+++ ub/src/app/service/card-list.ts
@@ -21,20 +21,6 @@ export class CardList {
       }
     ),
     new Card(
-      "Arsenic",
-      "Proxy",
-      "Arsenic is a simple, sleek, and efficient proxy. (Does not work_",
-      {
-        src: "/images/broken.png",
-        alt: "Placeholder icon for no image"
-      },
-      {
-        text: "Go",
-        link: "https://arsenic.ub.phuri.cc",
-        disabled: true
-      }
-    ),
-    new Card(
       "DogeUB",
       "AIO",
       "Doge Unblocker is a simple, yet nice looking proxy with some games.",
